9cc2e78685ebe03aa7ce86074b2a98c68b56d459
Bowen Wang <bowensw@uci.edu>
2025-06-16 03:19:19 -0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // Uncomment and set your repository name if deploying to GitHub Pages
+  // basePath: '/personal_website',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
